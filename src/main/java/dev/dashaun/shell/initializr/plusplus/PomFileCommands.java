6732cd06348aa058dfa86f0fc2b9a0cb2d6743a9
DaShaun <dashaun@dashaun.com>
2022-04-20 18:34:37 -0500

    Update project name

--- src/main/java/dev/dashaun/shell/initializr/plusplus/PomFileCommands.java
+++ src/main/java/dev/dashaun/shell/initializr/plusplus/PomFileCommands.java
@@ -4,10 +4,8 @@ import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
-import org.springframework.shell.standard.ShellCommandGroup;
-import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
-import org.springframework.shell.standard.ShellOption;
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.*;
 
 import java.io.File;
 import java.io.FileReader;
@@ -20,28 +18,40 @@ public class PomFileCommands {
 
     private final static File POM_FILE = new File("./pom.xml");
 
-    @ShellMethod("Update the artifact version")
-    public String artifactVersion(@ShellOption(defaultValue="0") String version) {
-        if (pomFile() && updateArtifactVersion(version)) {
-            return String.format("Successfully set artifact version to '%s'", version);
-        }
-        return "There was a problem updating the artifact version";
-    }
-
-    private boolean pomFile() {
-        return POM_FILE.exists();
-    }
-
-    private boolean updateArtifactVersion(String artifactVersion){
+    @ShellMethod("Update the project version")
+    @ShellMethodAvailability("pomFile")
+    public String projectVersion(@ShellOption(defaultValue="0") String version) {
         try {
             MavenXpp3Reader reader = new MavenXpp3Reader();
             Model model = reader.read(new FileReader(POM_FILE));
-            model.setVersion(artifactVersion);
+            model.setVersion(version);
             MavenXpp3Writer writer = new MavenXpp3Writer();
             writer.write(new FileWriter(POM_FILE), model);
         } catch (XmlPullParserException | IOException e){
-            return false;
+            return "There was a problem updating the project version.";
         }
-        return true;
+        return String.format("Successfully set project version to '%s'", version);
     }
+
+    @ShellMethod("Update the project name")
+    @ShellMethodAvailability("pomFile")
+    public String projectName(@ShellOption(defaultValue="${project.groupId}:${project.artifactId}") String name){
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            Model model = reader.read(new FileReader(POM_FILE));
+            model.setName(name);
+            MavenXpp3Writer writer = new MavenXpp3Writer();
+            writer.write(new FileWriter(POM_FILE), model);
+        } catch (XmlPullParserException | IOException e){
+            return "There was a problem updating the project name.";
+        }
+        return String.format("Successfully set project name to '%s'", name);
+    }
+
+    public Availability pomFile() {
+        return POM_FILE.exists()
+                ? Availability.available()
+                : Availability.unavailable(String.format("%s does not exist",POM_FILE.getName()));
+    }
+
 }
